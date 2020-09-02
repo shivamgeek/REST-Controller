68e9e5ff63424a29d9e8ed17bb26d8553b111100
Shivam Aggarwal <shivam01101995@gmail.com>
2020-09-02 21:43:33 +0530

    Add Java Annotation based spring_config file

---
+++ src/main/java/com/shivam/config/spring_config.java
@@ -0,0 +1,12 @@
+package com.shivam.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.shivam")
+public class spring_config {
+
+}
